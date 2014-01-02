7798861eed3d662101eb25ff66a64b7ea241112b
Taeho Kim <dittos@gmail.com>
2014-01-03 00:10:22 +0900

    Run crawler worker thread

--- EarthReader/helpers.py
+++ EarthReader/helpers.py
@@ -1,20 +1,3 @@
-from libearth.feed import Link
-from libearth.parser.heuristic import get_format
-
-def parse_feed(feed_xml, feed_url, content_type):
-    parser = get_format(feed_xml)
-    feed, crawler_hints = parser(feed_xml, feed_url)
-    self_uri = None
-    for link in feed.links:
-        if link.relation == 'self':
-            self_uri = link.uri
-    if not self_uri:
-        feed.links.append(Link(relation='self', uri=feed_url,
-                               mimetype=content_type))
-    feed.entries = sorted(feed.entries, key=lambda entry: entry.updated_at,
-                          reverse=True)
-    return feed_url, feed, crawler_hints
-
 # Fake multiprocessing module
 import sys, types
 mod = types.ModuleType('multiprocessing')
@@ -22,9 +5,14 @@ mod.cpu_count = lambda: 1
 sys.modules[mod.__name__] = mod
 
 from waitress.server import create_server
-from earthreader.web.app import app
+from earthreader.web.app import app, spawn_worker
+
+app._worker_running = False
 
 def new_server(repo_path, session_id):
     app.config['REPOSITORY'] = 'file://' + repo_path
     app.config['SESSION_ID'] = session_id
+    if not app._worker_running:
+        spawn_worker()
+        app._worker_running = True
     return create_server(app, port=0)
